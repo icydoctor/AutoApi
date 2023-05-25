0ba7595135ee134a797bd6909bc2c1a867626477
icydoctor <123283872+icydoctor@users.noreply.github.com>
2023-05-25 17:21:14 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -101,7 +101,7 @@ def main():
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
             num1+=1
             print('20调用成功'+str(num1)+'次')
-            print('此次运行结束时间为 :', localtime)
+     
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',headers=headers).status_code == 200:
             num1+=1
             print("21调用成功"+str(num1)+'次')
